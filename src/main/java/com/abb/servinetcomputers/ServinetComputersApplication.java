3cace38d0436083f3e83db8e0177b3adc03d2d7e
Alben Bustamante <albenbustamante@gmail.com>
2023-03-08 00:51:00 -0500

    first commit

---
+++ src/main/java/com/abb/servinetcomputers/ServinetComputersApplication.java
@@ -0,0 +1,13 @@
+package com.abb.servinetcomputers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServinetComputersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServinetComputersApplication.class, args);
+	}
+
+}
