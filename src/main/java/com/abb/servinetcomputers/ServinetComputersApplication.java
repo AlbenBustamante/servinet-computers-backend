5a81852239fc415b7a1053ec2c03cdc795f3eaf5
Alben Bustamante <albenbustamante@gmail.com>
2023-03-14 01:12:55 -0500

    entry point

--- src/main/java/com/abb/servinetcomputers/ServinetComputersApplication.java
+++ src/main/java/com/abb/servinetcomputers/ServinetComputersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServinetComputersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServinetComputersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServinetComputersApplication.class, args);
+    }
 
 }
